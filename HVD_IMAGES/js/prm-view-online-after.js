4d4daf01d6c9a03bf01416a7bfe705c4535d98c1
sam san <khmereshop@yahoo.com>
2017-11-02 16:08:38 -0400

    move truncatefilter

--- HVD_IMAGES/js/prm-view-online-after.js
+++ HVD_IMAGES/js/prm-view-online-after.js
@@ -120,4 +120,28 @@ angular.module('viewCustom')
 
 
 
+// truncate word to limit 60 characters
+angular.module('viewCustom').filter('truncatefilter',function () {
+    return function (str) {
+        var newstr=str;
+        var index=45;
+        if(str) {
+            if (str.length > 45) {
+                newstr = str.substring(0, 45);
+                for (var i = newstr.length; i > 20; i--) {
+                    var text = newstr.substring(i - 1, i);
+                    if (text === ' ') {
+                        index = i;
+                        i = 20;
+                    }
+                }
+                newstr = str.substring(0, index) + '...';
 
+            }
+
+        }
+
+        return newstr;
+    }
+
+});
